9c981e0e655504168f1d1b8c4a3a382f34d91baa
gabimeneguels <gabimeneguels@gmail.com>
2026-02-24 17:00:47 -0300

    MAIN.C

--- MAIN.C
+++ MAIN.C
@@ -1,1 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+
+// Estrutura da carta
+typedef struct {
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+    float densidade;
+} Carta;
+
+int main() {
+
+    Carta carta1 = {"Brasil", 203000000, 8515767.0, 1.84, 20, 23.8};
+    Carta carta2 = {"Canada", 38000000, 9984670.0, 1.99, 15, 3.8};
+
+    int opcao;
+
+    printf("===== SUPER TRUNFO =====\n");
+    printf("Escolha o atributo para comparar:\n");
+    printf("1 - Populacao\n");
+    printf("2 - Area\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turisticos\n");
+    printf("5 - Densidade Demografica\n");
+    printf("Opcao: ");
+    scanf("%d", &opcao);
+
+    printf("\nComparando %s x %s\n", carta1.nome, carta2.nome);
+
+    switch(opcao) {
+
+        case 1:
+            printf("Atributo: Populacao\n");
+            printf("%s: %d\n", carta1.nome, carta1.populacao);
+            printf("%s: %d\n", carta2.nome, carta2.populacao);
+
+            if(carta1.populacao > carta2.populacao) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if(carta2.populacao > carta1.populacao) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 2:
+            printf("Atributo: Area\n");
+            printf("%s: %.2f\n", carta1.nome, carta1.area);
+            printf("%s: %.2f\n", carta2.nome, carta2.area);
+
+            if(carta1.area > carta2.area) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if(carta2.area > carta1.area) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 3:
+            printf("Atributo: PIB\n");
+            printf("%s: %.2f trilhoes\n", carta1.nome, carta1.pib);
+            printf("%s: %.2f trilhoes\n", carta2.nome, carta2.pib);
+
+            if(carta1.pib > carta2.pib) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if(carta2.pib > carta1.pib) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 4:
+            printf("Atributo: Pontos Turisticos\n");
+            printf("%s: %d\n", carta1.nome, carta1.pontosTuristicos);
+            printf("%s: %d\n", carta2.nome, carta2.pontosTuristicos);
+
+            if(carta1.pontosTuristicos > carta2.pontosTuristicos) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if(carta2.pontosTuristicos > carta1.pontosTuristicos) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 5:
+            printf("Atributo: Densidade Demografica\n");
+            printf("%s: %.2f\n", carta1.nome, carta1.densidade);
+            printf("%s: %.2f\n", carta2.nome, carta2.densidade);
+
+            if(carta1.densidade < carta2.densidade) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if(carta2.densidade < carta1.densidade) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        default:
+            printf("Opcao invalida!\n");
+    }
+
+    return 0;
+}
 
